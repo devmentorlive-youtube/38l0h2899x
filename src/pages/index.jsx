31ff12656c045e0f494bda8e2ca25732a7823899
Mark Tellez <mark@devmentor.live>
2022-09-08 10:27:30 -0500

    multi select

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export default function Homepage() {
+  const choices = ["choice 1", "choice 2", "choice 3", "choice 4"];
+  const [selectedChoices, setSelectedChoices] = useState([]);
+
+  function toggle(array, item) {
+    return array.includes(item)
+      ? array.filter((_item) => _item !== item)
+      : [...array, item];
+  }
+
   return (
     <div className="mt-16 container mx-auto">
-      <h1>Hello world.</h1>
+      <div className="border p-2">{selectedChoices}</div>
+      {choices.map((choice) => (
+        <div onClick={() => setSelectedChoices((prev) => toggle(prev, choice))}>
+          {choice}
+        </div>
+      ))}
     </div>
   );
 }
